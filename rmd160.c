681b12c64978917dc54aa0d5ff7810e380430e81
Joey Hewitt <joey@joeyhewitt.com>
2014-07-08 00:11:35 -0600

    rmd160: roll up them loops!
    61% savings in z80 generated code size. Haven't taken a look at it to
    see what more can be done yet.

--- rmd160.c
+++ rmd160.c
@@ -13,6 +13,9 @@
  *
  * This source has been radically overhauled to be portable and work within
  * the LibTomCrypt API by Tom St Denis
+ *
+ * Radically overhauled again, by putting everything into runtime loops so that the generated
+ * code is smaller.  By Joey Hewitt.
  */
 #include "rmd160.h"
 
@@ -27,309 +30,130 @@ static mr_unsign32 ROLc(mr_unsign32 x, unsigned char n) {
 	return (((x) << (n)) | ((x) >> (32-(n))));
 }
 
-/* the five basic functions F(), G() and H() */
-// SDCC seems to compile smaller with these left as macros
-#define F(x, y, z)        ((x) ^ (y) ^ (z))
-#define G(x, y, z)        (((x) & (y)) | (~(x) & (z)))
-#define H(x, y, z)        (((x) | ~(y)) ^ (z))
-#define I(x, y, z)        (((x) & (z)) | ((y) & ~(z)))
-#define J(x, y, z)        ((x) ^ ((y) | ~(z)))
-
-/* the ten basic operations FF() through III() */
-#ifndef TI8X
-inline
-#endif
-static void FF(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += F(b, *pc, d) + x;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
+/* the five basic functions F() through J() */
+static mr_unsign32 F(mr_unsign32 x, mr_unsign32 y, mr_unsign32 z) {
+	return (x) ^ (y) ^ (z);
+}
+static mr_unsign32 G(mr_unsign32 x, mr_unsign32 y, mr_unsign32 z) {
+	return ((x) & (y)) | (~(x) & (z));
+}
+static mr_unsign32 H(mr_unsign32 x, mr_unsign32 y, mr_unsign32 z) {
+	return ((x) | ~(y)) ^ (z);
+}
+static mr_unsign32 I(mr_unsign32 x, mr_unsign32 y, mr_unsign32 z) {
+	return ((x) & (z)) | ((y) & ~(z));
+}
+static mr_unsign32 J(mr_unsign32 x, mr_unsign32 y, mr_unsign32 z) {
+	return (x) ^ ((y) | ~(z));
 }
 
-#ifndef TI8X
-inline
-#endif
-static void GG(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += G(b, *pc, d) + x + 0x5a827999UL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
+unsigned char N(unsigned char i, unsigned char n) {
+	signed char r = (n-i)%5;
+	if (r < 0) {
+		return 5 + r;
+	} else {
+		return r;
+	}
 }
-
-#ifndef TI8X
-inline
-#endif
-static void HH(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += H(b, *pc, d) + x + 0x6ed9eba1UL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void II(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += I(b, *pc, d) + x + 0x8f1bbcdcUL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void JJ(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += J(b, *pc, d) + x + 0xa953fd4eUL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void FFF(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += F(b, *pc, d) + x;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void GGG(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += G(b, *pc, d) + x + 0x7a6d76e9UL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void HHH(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += H(b, *pc, d) + x + 0x6d703ef3UL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void III(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += I(b, *pc, d) + x + 0x5c4dd124UL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
-#ifndef TI8X
-inline
-#endif
-static void JJJ(mr_unsign32 *pa, mr_unsign32 b, mr_unsign32 *pc, mr_unsign32 d, mr_unsign32 e, mr_unsign32 x, unsigned char s) {
-	*pa += J(b, *pc, d) + x + 0x50a28be6UL;
-	*pa = ROLc(*pa, s) + e;
-	*pc = ROLc(*pc, 10);
-}
-
 
 void rmd160_compress(hash_state *md, unsigned char *buf) {
-	mr_unsign32 aa,bb,cc,dd,ee,aaa,bbb,ccc,ddd,eee,X[16];
-	int i;
+	mr_unsign32 _aa[5];
+	mr_unsign32 _aaa[5];
+	mr_unsign32 *X = (mr_unsign32*)buf;
 
-	/* load words X */
-	for (i = 0; i < 16; i++) {
-		X[i] = ((mr_unsign32*)buf)[i];
-	}
+	unsigned char i;
 
 	/* load state */
-	aa = aaa = md->rmd160.out[0];
-	bb = bbb = md->rmd160.out[1];
-	cc = ccc = md->rmd160.out[2];
-	dd = ddd = md->rmd160.out[3];
-	ee = eee = md->rmd160.out[4];
+	_aa[0] = _aaa[0] = md->rmd160.out[0];
+	_aa[1] = _aaa[1] = md->rmd160.out[1];
+	_aa[2] = _aaa[2] = md->rmd160.out[2];
+	_aa[3] = _aaa[3] = md->rmd160.out[3];
+	_aa[4] = _aaa[4] = md->rmd160.out[4];
 
-	/* round 1 */
-	FF(&aa, bb, &cc, dd, ee, X[ 0], 11);
-	FF(&ee, aa, &bb, cc, dd, X[ 1], 14);
-	FF(&dd, ee, &aa, bb, cc, X[ 2], 15);
-	FF(&cc, dd, &ee, aa, bb, X[ 3], 12);
-	FF(&bb, cc, &dd, ee, aa, X[ 4],  5);
-	FF(&aa, bb, &cc, dd, ee, X[ 5],  8);
-	FF(&ee, aa, &bb, cc, dd, X[ 6],  7);
-	FF(&dd, ee, &aa, bb, cc, X[ 7],  9);
-	FF(&cc, dd, &ee, aa, bb, X[ 8], 11);
-	FF(&bb, cc, &dd, ee, aa, X[ 9], 13);
-	FF(&aa, bb, &cc, dd, ee, X[10], 14);
-	FF(&ee, aa, &bb, cc, dd, X[11], 15);
-	FF(&dd, ee, &aa, bb, cc, X[12],  6);
-	FF(&cc, dd, &ee, aa, bb, X[13],  7);
-	FF(&bb, cc, &dd, ee, aa, X[14],  9);
-	FF(&aa, bb, &cc, dd, ee, X[15],  8);
+	{
+#define aa _aa[N(wordi, 0)]
+#define bb _aa[N(wordi, 1)]
+#define cc _aa[N(wordi, 2)]
+#define dd _aa[N(wordi, 3)]
+#define ee _aa[N(wordi, 4)]
+#define aaa _aaa[N(wordi, 0)]
+#define bbb _aaa[N(wordi, 1)]
+#define ccc _aaa[N(wordi, 2)]
+#define ddd _aaa[N(wordi, 3)]
+#define eee _aaa[N(wordi, 4)]
 
-	/* round 2 */
-	GG(&ee, aa, &bb, cc, dd, X[ 7],  7);
-	GG(&dd, ee, &aa, bb, cc, X[ 4],  6);
-	GG(&cc, dd, &ee, aa, bb, X[13],  8);
-	GG(&bb, cc, &dd, ee, aa, X[ 1], 13);
-	GG(&aa, bb, &cc, dd, ee, X[10], 11);
-	GG(&ee, aa, &bb, cc, dd, X[ 6],  9);
-	GG(&dd, ee, &aa, bb, cc, X[15],  7);
-	GG(&cc, dd, &ee, aa, bb, X[ 3], 15);
-	GG(&bb, cc, &dd, ee, aa, X[12],  7);
-	GG(&aa, bb, &cc, dd, ee, X[ 0], 12);
-	GG(&ee, aa, &bb, cc, dd, X[ 9], 15);
-	GG(&dd, ee, &aa, bb, cc, X[ 5],  9);
-	GG(&cc, dd, &ee, aa, bb, X[ 2], 11);
-	GG(&bb, cc, &dd, ee, aa, X[14],  7);
-	GG(&aa, bb, &cc, dd, ee, X[11], 13);
-	GG(&ee, aa, &bb, cc, dd, X[ 8], 12);
+		// rounds 6-11 are the "parallel rounds"
+		unsigned char rotn[10][16] = {
+			{ 11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8 },
+			{ 7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12 },
+			{ 11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5 },
+			{ 11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12 },
+			{ 9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6 },
+			{ 8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6 },
+			{ 9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11 },
+			{ 9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5 },
+			{ 15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8 },
+			{ 8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11 },
+		};
+		unsigned char wrdn[10][16] = {
+			{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, // XXX inline into code?
+			{ 7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8 },
+			{ 3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12 },
+			{ 1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2 },
+			{ 4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13 },
+			{ 5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12 },
+			{ 6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2 },
+			{ 15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13 },
+			{ 8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14 },
+			{ 12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11 },
+		};
+		mr_unsign32 (*funcs[5])(mr_unsign32, mr_unsign32, mr_unsign32) = {
+			F, G, H, I, J /* parallel rounds go in reverse order */
+		};
+		mr_unsign32 consts[10] = {
+			0x00000000UL, 0x5a827999UL, 0x6ed9eba1UL, 0x8f1bbcdcUL, 0xa953fd4eUL,
+			0x50a28be6UL, 0x5c4dd124UL, 0x6d703ef3UL, 0x7a6d76e9UL, 0x00000000UL
+		};
 
-	/* round 3 */
-	HH(&dd, ee, &aa, bb, cc, X[ 3], 11);
-	HH(&cc, dd, &ee, aa, bb, X[10], 13);
-	HH(&bb, cc, &dd, ee, aa, X[14],  6);
-	HH(&aa, bb, &cc, dd, ee, X[ 4],  7);
-	HH(&ee, aa, &bb, cc, dd, X[ 9], 14);
-	HH(&dd, ee, &aa, bb, cc, X[15],  9);
-	HH(&cc, dd, &ee, aa, bb, X[ 8], 13);
-	HH(&bb, cc, &dd, ee, aa, X[ 1], 15);
-	HH(&aa, bb, &cc, dd, ee, X[ 2], 14);
-	HH(&ee, aa, &bb, cc, dd, X[ 7],  8);
-	HH(&dd, ee, &aa, bb, cc, X[ 0], 13);
-	HH(&cc, dd, &ee, aa, bb, X[ 6],  6);
-	HH(&bb, cc, &dd, ee, aa, X[13],  5);
-	HH(&aa, bb, &cc, dd, ee, X[11], 12);
-	HH(&ee, aa, &bb, cc, dd, X[ 5],  7);
-	HH(&dd, ee, &aa, bb, cc, X[12],  5);
+		unsigned char round, wordi = 0;
 
-	/* round 4 */
-	II(&cc, dd, &ee, aa, bb, X[ 1], 11);
-	II(&bb, cc, &dd, ee, aa, X[ 9], 12);
-	II(&aa, bb, &cc, dd, ee, X[11], 14);
-	II(&ee, aa, &bb, cc, dd, X[10], 15);
-	II(&dd, ee, &aa, bb, cc, X[ 0], 14);
-	II(&cc, dd, &ee, aa, bb, X[ 8], 15);
-	II(&bb, cc, &dd, ee, aa, X[12],  9);
-	II(&aa, bb, &cc, dd, ee, X[ 4],  8);
-	II(&ee, aa, &bb, cc, dd, X[13],  9);
-	II(&dd, ee, &aa, bb, cc, X[ 3], 14);
-	II(&cc, dd, &ee, aa, bb, X[ 7],  5);
-	II(&bb, cc, &dd, ee, aa, X[15],  6);
-	II(&aa, bb, &cc, dd, ee, X[14],  8);
-	II(&ee, aa, &bb, cc, dd, X[ 5],  6);
-	II(&dd, ee, &aa, bb, cc, X[ 6],  5);
-	II(&cc, dd, &ee, aa, bb, X[ 2], 12);
+		/* rounds 1-5 */
+		for (round = 0; round < 5; round++) {
+			for (i = 0; i < 16; i++, wordi++) {
+				aa += funcs[round](bb, cc, dd) + X[wrdn[round][i]] + consts[round];
+				aa = ROLc(aa, rotn[round][i]) + ee;
+				cc = ROLc(cc, 10);
+			}
+		}
 
-	/* round 5 */
-	JJ(&bb, cc, &dd, ee, aa, X[ 4],  9);
-	JJ(&aa, bb, &cc, dd, ee, X[ 0], 15);
-	JJ(&ee, aa, &bb, cc, dd, X[ 5],  5);
-	JJ(&dd, ee, &aa, bb, cc, X[ 9], 11);
-	JJ(&cc, dd, &ee, aa, bb, X[ 7],  6);
-	JJ(&bb, cc, &dd, ee, aa, X[12],  8);
-	JJ(&aa, bb, &cc, dd, ee, X[ 2], 13);
-	JJ(&ee, aa, &bb, cc, dd, X[10], 12);
-	JJ(&dd, ee, &aa, bb, cc, X[14],  5);
-	JJ(&cc, dd, &ee, aa, bb, X[ 1], 12);
-	JJ(&bb, cc, &dd, ee, aa, X[ 3], 13);
-	JJ(&aa, bb, &cc, dd, ee, X[ 8], 14);
-	JJ(&ee, aa, &bb, cc, dd, X[11], 11);
-	JJ(&dd, ee, &aa, bb, cc, X[ 6],  8);
-	JJ(&cc, dd, &ee, aa, bb, X[15],  5);
-	JJ(&bb, cc, &dd, ee, aa, X[13],  6);
+		/* "parallel rounds" 1-5 */
+		for (; round < 10; round++) {
+			for (i = 0; i < 16; i++, wordi++) {
+				aaa += funcs[(9 - round)](bbb, ccc, ddd) + X[wrdn[round][i]] + consts[round];
+				aaa = ROLc(aaa, rotn[round][i]) + eee;
+				ccc = ROLc(ccc, 10);
+			}
+		}
 
-	/* parallel round 1 */
-	JJJ(&aaa, bbb, &ccc, ddd, eee, X[ 5],  8);
-	JJJ(&eee, aaa, &bbb, ccc, ddd, X[14],  9);
-	JJJ(&ddd, eee, &aaa, bbb, ccc, X[ 7],  9);
-	JJJ(&ccc, ddd, &eee, aaa, bbb, X[ 0], 11);
-	JJJ(&bbb, ccc, &ddd, eee, aaa, X[ 9], 13);
-	JJJ(&aaa, bbb, &ccc, ddd, eee, X[ 2], 15);
-	JJJ(&eee, aaa, &bbb, ccc, ddd, X[11], 15);
-	JJJ(&ddd, eee, &aaa, bbb, ccc, X[ 4],  5);
-	JJJ(&ccc, ddd, &eee, aaa, bbb, X[13],  7);
-	JJJ(&bbb, ccc, &ddd, eee, aaa, X[ 6],  7);
-	JJJ(&aaa, bbb, &ccc, ddd, eee, X[15],  8);
-	JJJ(&eee, aaa, &bbb, ccc, ddd, X[ 8], 11);
-	JJJ(&ddd, eee, &aaa, bbb, ccc, X[ 1], 14);
-	JJJ(&ccc, ddd, &eee, aaa, bbb, X[10], 14);
-	JJJ(&bbb, ccc, &ddd, eee, aaa, X[ 3], 12);
-	JJJ(&aaa, bbb, &ccc, ddd, eee, X[12],  6);
-
-	/* parallel round 2 */
-	III(&eee, aaa, &bbb, ccc, ddd, X[ 6],  9);
-	III(&ddd, eee, &aaa, bbb, ccc, X[11], 13);
-	III(&ccc, ddd, &eee, aaa, bbb, X[ 3], 15);
-	III(&bbb, ccc, &ddd, eee, aaa, X[ 7],  7);
-	III(&aaa, bbb, &ccc, ddd, eee, X[ 0], 12);
-	III(&eee, aaa, &bbb, ccc, ddd, X[13],  8);
-	III(&ddd, eee, &aaa, bbb, ccc, X[ 5],  9);
-	III(&ccc, ddd, &eee, aaa, bbb, X[10], 11);
-	III(&bbb, ccc, &ddd, eee, aaa, X[14],  7);
-	III(&aaa, bbb, &ccc, ddd, eee, X[15],  7);
-	III(&eee, aaa, &bbb, ccc, ddd, X[ 8], 12);
-	III(&ddd, eee, &aaa, bbb, ccc, X[12],  7);
-	III(&ccc, ddd, &eee, aaa, bbb, X[ 4],  6);
-	III(&bbb, ccc, &ddd, eee, aaa, X[ 9], 15);
-	III(&aaa, bbb, &ccc, ddd, eee, X[ 1], 13);
-	III(&eee, aaa, &bbb, ccc, ddd, X[ 2], 11);
-
-	/* parallel round 3 */
-	HHH(&ddd, eee, &aaa, bbb, ccc, X[15],  9);
-	HHH(&ccc, ddd, &eee, aaa, bbb, X[ 5],  7);
-	HHH(&bbb, ccc, &ddd, eee, aaa, X[ 1], 15);
-	HHH(&aaa, bbb, &ccc, ddd, eee, X[ 3], 11);
-	HHH(&eee, aaa, &bbb, ccc, ddd, X[ 7],  8);
-	HHH(&ddd, eee, &aaa, bbb, ccc, X[14],  6);
-	HHH(&ccc, ddd, &eee, aaa, bbb, X[ 6],  6);
-	HHH(&bbb, ccc, &ddd, eee, aaa, X[ 9], 14);
-	HHH(&aaa, bbb, &ccc, ddd, eee, X[11], 12);
-	HHH(&eee, aaa, &bbb, ccc, ddd, X[ 8], 13);
-	HHH(&ddd, eee, &aaa, bbb, ccc, X[12],  5);
-	HHH(&ccc, ddd, &eee, aaa, bbb, X[ 2], 14);
-	HHH(&bbb, ccc, &ddd, eee, aaa, X[10], 13);
-	HHH(&aaa, bbb, &ccc, ddd, eee, X[ 0], 13);
-	HHH(&eee, aaa, &bbb, ccc, ddd, X[ 4],  7);
-	HHH(&ddd, eee, &aaa, bbb, ccc, X[13],  5);
-
-	/* parallel round 4 */
-	GGG(&ccc, ddd, &eee, aaa, bbb, X[ 8], 15);
-	GGG(&bbb, ccc, &ddd, eee, aaa, X[ 6],  5);
-	GGG(&aaa, bbb, &ccc, ddd, eee, X[ 4],  8);
-	GGG(&eee, aaa, &bbb, ccc, ddd, X[ 1], 11);
-	GGG(&ddd, eee, &aaa, bbb, ccc, X[ 3], 14);
-	GGG(&ccc, ddd, &eee, aaa, bbb, X[11], 14);
-	GGG(&bbb, ccc, &ddd, eee, aaa, X[15],  6);
-	GGG(&aaa, bbb, &ccc, ddd, eee, X[ 0], 14);
-	GGG(&eee, aaa, &bbb, ccc, ddd, X[ 5],  6);
-	GGG(&ddd, eee, &aaa, bbb, ccc, X[12],  9);
-	GGG(&ccc, ddd, &eee, aaa, bbb, X[ 2], 12);
-	GGG(&bbb, ccc, &ddd, eee, aaa, X[13],  9);
-	GGG(&aaa, bbb, &ccc, ddd, eee, X[ 9], 12);
-	GGG(&eee, aaa, &bbb, ccc, ddd, X[ 7],  5);
-	GGG(&ddd, eee, &aaa, bbb, ccc, X[10], 15);
-	GGG(&ccc, ddd, &eee, aaa, bbb, X[14],  8);
-
-	/* parallel round 5 */
-	FFF(&bbb, ccc, &ddd, eee, aaa, X[12] ,  8);
-	FFF(&aaa, bbb, &ccc, ddd, eee, X[15] ,  5);
-	FFF(&eee, aaa, &bbb, ccc, ddd, X[10] , 12);
-	FFF(&ddd, eee, &aaa, bbb, ccc, X[ 4] ,  9);
-	FFF(&ccc, ddd, &eee, aaa, bbb, X[ 1] , 12);
-	FFF(&bbb, ccc, &ddd, eee, aaa, X[ 5] ,  5);
-	FFF(&aaa, bbb, &ccc, ddd, eee, X[ 8] , 14);
-	FFF(&eee, aaa, &bbb, ccc, ddd, X[ 7] ,  6);
-	FFF(&ddd, eee, &aaa, bbb, ccc, X[ 6] ,  8);
-	FFF(&ccc, ddd, &eee, aaa, bbb, X[ 2] , 13);
-	FFF(&bbb, ccc, &ddd, eee, aaa, X[13] ,  6);
-	FFF(&aaa, bbb, &ccc, ddd, eee, X[14] ,  5);
-	FFF(&eee, aaa, &bbb, ccc, ddd, X[ 0] , 15);
-	FFF(&ddd, eee, &aaa, bbb, ccc, X[ 3] , 13);
-	FFF(&ccc, ddd, &eee, aaa, bbb, X[ 9] , 11);
-	FFF(&bbb, ccc, &ddd, eee, aaa, X[11] , 11);
+#undef aa
+#undef bb
+#undef cc
+#undef dd
+#undef ee
+#undef aaa
+#undef bbb
+#undef ccc
+#undef ddd
+#undef eee
+	}
 
 	/* combine results */
-	ddd += cc + md->rmd160.out[1];		 /* final result for md->rmd160.out[0] */
-	md->rmd160.out[1] = md->rmd160.out[2] + dd + eee;
-	md->rmd160.out[2] = md->rmd160.out[3] + ee + aaa;
-	md->rmd160.out[3] = md->rmd160.out[4] + aa + bbb;
-	md->rmd160.out[4] = md->rmd160.out[0] + bb + ccc;
-	md->rmd160.out[0] = ddd;
+	_aaa[3] += _aa[2] + md->rmd160.out[1];		 /* final result for md->rmd160.out[0] */
+	md->rmd160.out[1] = md->rmd160.out[2] + _aa[3] + _aaa[4];
+	md->rmd160.out[2] = md->rmd160.out[3] + _aa[4] + _aaa[0];
+	md->rmd160.out[3] = md->rmd160.out[4] + _aa[0] + _aaa[1];
+	md->rmd160.out[4] = md->rmd160.out[0] + _aa[1] + _aaa[2];
+	md->rmd160.out[0] = _aaa[3];
 }
 
 /**
