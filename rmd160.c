3aac0d4ba752fd0c3d886856d7ee8e507c2e902a
Joey Hewitt <joey@joeyhewitt.com>
2014-07-08 00:52:24 -0600

    rmd160: make constants static const so that sdcc can put them in a data section, instead of generating code to build them

--- rmd160.c
+++ rmd160.c
@@ -85,7 +85,7 @@ void rmd160_compress(hash_state *md, unsigned char *buf) {
 #define eee _aaa[N(wordi, 4)]
 
 		// rounds 6-11 are the "parallel rounds"
-		unsigned char rotn[10][16] = {
+		static const unsigned char rotn[10][16] = {
 			{ 11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8 },
 			{ 7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12 },
 			{ 11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5 },
@@ -97,7 +97,7 @@ void rmd160_compress(hash_state *md, unsigned char *buf) {
 			{ 15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8 },
 			{ 8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11 },
 		};
-		unsigned char wrdn[10][16] = {
+		static const unsigned char wrdn[10][16] = {
 			{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, // XXX inline into code?
 			{ 7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8 },
 			{ 3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12 },
@@ -109,10 +109,10 @@ void rmd160_compress(hash_state *md, unsigned char *buf) {
 			{ 8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14 },
 			{ 12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11 },
 		};
-		mr_unsign32 (*funcs[5])(mr_unsign32, mr_unsign32, mr_unsign32) = {
+		static mr_unsign32 (* const funcs[5])(mr_unsign32, mr_unsign32, mr_unsign32) = {
 			F, G, H, I, J /* parallel rounds go in reverse order */
 		};
-		mr_unsign32 consts[10] = {
+		static const mr_unsign32 consts[10] = {
 			0x00000000UL, 0x5a827999UL, 0x6ed9eba1UL, 0x8f1bbcdcUL, 0xa953fd4eUL,
 			0x50a28be6UL, 0x5c4dd124UL, 0x6d703ef3UL, 0x7a6d76e9UL, 0x00000000UL
 		};
